39892ca6f9b0a5ce9ab86d3181dc587e8ecc135a
Andikarya17 <andikaryaputra17@gmail.com>
2024-03-14 15:17:20 +0700

    commit 2

--- 214_InsertionSort/214_InsertionSort.cpp
+++ 214_InsertionSort/214_InsertionSort.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int i, j, n, temp;
 int arr[20];
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+
 
 
